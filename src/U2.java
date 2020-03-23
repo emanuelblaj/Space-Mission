28c601d7124196a35fd1793f40da2e9072ecf8cc
Manue <emanuelblaj93@gmail.com>
2020-03-23 14:49:10 +0200

    Add in U2 classconstructors, land and launch methods

--- src/U2.java
+++ src/U2.java
@@ -1,15 +1,35 @@
+import java.util.Random;
 
 public class U2 extends Rocket {
-	@Override
-	public boolean land() {
-		// TODO Auto-generated method stub
-		return super.land();
+	
+//	Constructors
+	public U2() {
+		this.setCost(120);
+		this.setWeight(18000);
+		this.setMaxWeight(29000);
+		this.setLaunchExp(0.04);
+		this.setLandCrash(0.08);
+		this.setCargo_carried(0);
+		this.setCargo_limit(this.getMaxWeight() - this.getWeight());
 	}
 	
+//	Returns launch explosion probability outcome
 	@Override
 	public boolean launch() {
-		// TODO Auto-generated method stub
-		return super.launch();
+		if(new Random().nextDouble() <= 0.04 * (getCargo_carried() / getCargo_limit())) {
+			return false;
+		} else 
+			return true;
+	}
+	
+//	Returns crash landing probability outcome
+	@Override
+	public boolean land() {
+		if(new Random().nextDouble() <= 0.08 *(getCargo_carried() /getCargo_limit())) {
+			return false;
+			
+		} else
+			return true;
 	}
 
 }
