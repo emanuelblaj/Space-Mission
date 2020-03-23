2035d2b2387c9aa1fb9538fe883c66d80627c5f9
Manue <emanuelblaj93@gmail.com>
2020-03-23 14:36:25 +0200

    Create constructor with variable and methods launch and land with their limitation in U1 Class

--- src/U1.java
+++ src/U1.java
@@ -1,19 +1,38 @@
+import java.util.Random;
 
+// U1 rocket
 public class U1 extends Rocket {
-	@Override
-	public void setCost(int cost) {
-		// TODO Auto-generated method stub
-		super.setCost(cost);
+	
+//	Constructor
+	public U1() {
+		this.setCost(100);
+		this.setWeight(10000);
+		this.setMaxWeight(18000);
+		this.setLaunchExp(0.05);
+		this.setLandCrash(0.01);
+		this.setCargo_carried(0);
+		this.setCargo_limit(this.getMaxWeight() - this.getWeight());
+		
 	}
+	
 
-	@Override
-	public boolean land() {
-		// TODO Auto-generated method stub
-		return super.land();
-	}
+	
+//	Returns launch explosion probability outcome
 	@Override
 	public boolean launch() {
-		// TODO Auto-generated method stub
-		return super.launch();
+		if(new Random().nextDouble() <= 0.05 *(getCargo_carried() / getCargo_limit())) {
+			return false;
+		} else
+			return true;
+		
+	}
+	
+//	Returns crash landing probability outcome
+	@Override
+	public boolean land() {
+		if(new Random().nextDouble() <= 0.01 *(getCargo_carried() / getCargo_limit())) {
+			return false;
+		} else
+			return true;
 	}
 }
